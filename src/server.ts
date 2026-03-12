7d84dd98cc12992a304d7e191879183a5b963596
thomnico <thomnico@googlemail.com>
2026-03-12 22:19:31 +0100

    chore: bump version to 0.7.2

--- src/server.ts
+++ src/server.ts
@@ -36,7 +36,7 @@ export class DebugMcpServer {
   private createSession(): { server: McpServer; transport: StreamableHTTPServerTransport; notificationManager?: NotificationManager } {
     const server = new McpServer({
       name: EXTENSION_ID,
-      version: "0.7.1",
+      version: "0.7.2",
     });
 
     registerAllTools(server, this.adapter);
